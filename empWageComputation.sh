091c6e22105bac67146c947527f0f06e3cee7599
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-24 18:20:28 +0530

    Added employee absent or present

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,16 @@
 
 echo "Welcome to shell script"
 
+IS_PRESENT=1
+
+empCheck=$(( RANDOM%2 ))
+
+
+if [ $empCheck -eq $IS_PRESENT ]
+then
+echo "employee is present"
+else
+echo "employess is absent"
+fi
+
+
