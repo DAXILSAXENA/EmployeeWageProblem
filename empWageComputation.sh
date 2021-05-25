b829aa647ce75d32a770246b1cb2b78b76c0016a
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-25 13:15:46 +0530

    Added Part Time Employee and Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,25 @@
 echo "Welcome to shell script"
 
 # constants
-IS_PRESENT=1
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 
 # variable
-empCheck=$(( RANDOM % 2 ))
+empCheck=$(( RANDOM % 3 )) # 0/1/2
 
 # selection
 
-if [ $empCheck -eq $IS_PRESENT ]
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
 then
-        empHrs=8
-        salary=$(( EMP_RATE_PER_HR=20 * empHrs )) #action 1
+        empHrs=8 # action 1
+elif [ $empCheck -eq $IS_PRESENT_PART_TIME ]
+then
+        empHrs=4 # action 2
 else
-        salary=0 #action 2
+        empHrs=0 # action 3
 fi
+salary=$(( EMP_RATE_PER_HR * empHrs ))
 
 
 
