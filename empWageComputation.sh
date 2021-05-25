351e0f46e4500b446a8854f424a376cbb99b325c
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-25 13:46:16 +0530

    Added Wages For a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,23 +6,23 @@ echo "Welcome to shell script"
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
+totalWorkingHours=0
 
-# variable
+for (( Day=1; Day<=20; Day++ ))
+do
+
 empCheck=$(( RANDOM % 3 )) # 0/1/2
-
-# selection
 
 case $empCheck in
         $IS_PRESENT_FULL_TIME) empHrs=8;;
         $IS_PRESENT_PART_TIME) empHrs=4;;
         *) empHrs=0;;
 esac
-salary=$(( EMP_RATE_PER_HR * empHrs ))
-
-
-
-
-
-
+        totalWorkingHours=$(( totalWorkingHours + empHrs ))
+        salary=$(( EMP_RATE_PER_HR * empHrs ))
+        echo $salary
+done
+totalsalary=$((totalWorkingHours*EMP_RATE_PER_HR ))
+echo totalsalary : $totalsalary
 
 
